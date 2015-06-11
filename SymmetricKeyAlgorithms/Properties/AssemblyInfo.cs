73edd72cb16f5f6fd8ec92e7a74b6daf245c57b3
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2015-06-11 21:23:59 +0900

    Update AssemblyInfo.cs

--- SymmetricKeyAlgorithms/Properties/AssemblyInfo.cs
+++ SymmetricKeyAlgorithms/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SymmetricKeyAlgorithmTutorial")]
+[assembly: AssemblyTitle("SymmetricKeyAlgorithms")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SymmetricKeyAlgorithmTutorial")]
+[assembly: AssemblyProduct("SymmetricKeyAlgorithms")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
