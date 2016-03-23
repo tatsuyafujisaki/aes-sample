c97590bda9e977f924a897460a4e52d1b2bb37fe
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2016-03-23 19:02:21 +0900

    Dispose MemoryStream.

--- SymmetricKeyAlgorithms/Program.cs
+++ SymmetricKeyAlgorithms/Program.cs
@@ -1,41 +1,48 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace SymmetricKeyAlgorithms
 {
-    class Program
+    internal static class Program
     {
-        static string Encrypt(SymmetricAlgorithm aes, string s)
+        [SuppressMessage("Microsoft.Usage", "CA2202")]
+        private static string Encrypt(SymmetricAlgorithm sa, string s)
         {
-            var ms = new MemoryStream();
-            using (var cs = new CryptoStream(ms, aes.CreateEncryptor(aes.Key, aes.IV), CryptoStreamMode.Write))
+            using (var ms = new MemoryStream())
             {
-                var bytes = Encoding.ASCII.GetBytes(s);
-                cs.Write(bytes, 0, bytes.Length);
-                cs.FlushFinalBlock();
-                return Convert.ToBase64String(ms.ToArray());
+                using (var cs = new CryptoStream(ms, sa.CreateEncryptor(sa.Key, sa.IV), CryptoStreamMode.Write))
+                {
+                    var bytes = Encoding.ASCII.GetBytes(s);
+                    cs.Write(bytes, 0, bytes.Length);
+                    cs.FlushFinalBlock();
+                    return Convert.ToBase64String(ms.ToArray());
+                }
             }
         }
 
-        static string Decrypt(SymmetricAlgorithm aes, string encrypted)
+        [SuppressMessage("Microsoft.Usage", "CA2202")]
+        private static string Decrypt(SymmetricAlgorithm sa, string encrypted)
         {
-            var ms = new MemoryStream();
-            using (var cs = new CryptoStream(ms, aes.CreateDecryptor(aes.Key, aes.IV), CryptoStreamMode.Write))
+            using (var ms = new MemoryStream())
             {
-                var bytes = Convert.FromBase64String(encrypted);
-                cs.Write(bytes, 0, bytes.Length);
-                cs.FlushFinalBlock();
-                return Encoding.ASCII.GetString(ms.ToArray());
+                using (var cs = new CryptoStream(ms, sa.CreateDecryptor(sa.Key, sa.IV), CryptoStreamMode.Write))
+                {
+                    var bytes = Convert.FromBase64String(encrypted);
+                    cs.Write(bytes, 0, bytes.Length);
+                    cs.FlushFinalBlock();
+                    return Encoding.ASCII.GetString(ms.ToArray());
+                }
             }
         }
 
-        static void Main()
+        private static void Main()
         {
             const string original = "Here comes a string to encrypt.";
-            var encrypted = "";
-            var decrypted = "";
+            string encrypted;
+            string decrypted;
 
             // AesCryptoServiceProvider below can be replaced with ...
             // RijndaelManaged
@@ -44,7 +51,7 @@ namespace SymmetricKeyAlgorithms
             // TripleDESCryptoServiceProvider
             // https://msdn.microsoft.com/en-us/library/system.security.cryptography.symmetricalgorithm.aspx
 
-            using (var aes = new TripleDESCryptoServiceProvider())
+            using (var aes = new AesCryptoServiceProvider())
             {
                 encrypted = Encrypt(aes, original);
                 decrypted = Decrypt(aes, encrypted);
