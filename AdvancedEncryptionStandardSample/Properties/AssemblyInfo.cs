8e2a42242c7de07fe2e2c775792292e71371b7f1
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2017-05-04 14:36:29 +0900

    Update .NET Framework to v4.7

--- AdvancedEncryptionStandardSample/Properties/AssemblyInfo.cs
+++ AdvancedEncryptionStandardSample/Properties/AssemblyInfo.cs
@@ -11,5 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("c4f50f95-4385-4c8f-a596-b331af495aa6")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
