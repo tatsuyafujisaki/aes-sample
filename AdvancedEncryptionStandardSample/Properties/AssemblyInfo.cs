8ce7fb8293ea0dcf9810bfd562646386af9ecd69
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2018-03-02 23:36:40 +0900

    Update .NET Framework to 4.7.1

--- AdvancedEncryptionStandardSample/Properties/AssemblyInfo.cs
+++ AdvancedEncryptionStandardSample/Properties/AssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("c4f50f95-4385-4c8f-a596-b331af495aa6")]
-[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
